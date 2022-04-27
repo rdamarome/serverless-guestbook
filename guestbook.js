01dc53127fa5726256b0f35e142f2442dbdbe2fe
rdamarome <102402013+rdamarome@users.noreply.github.com>
2022-04-27 17:05:46 -0500

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://68888add.us-east.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
